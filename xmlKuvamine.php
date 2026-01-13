9e87c24ea07bd332638073c3502c4ce03d0881fa
Artur Sein <seinartur@gmail.com>
2026-01-13 13:47:32 +0200

    1 õpilase kogu info kuvamine

--- xmlKuvamine.php
+++ xmlKuvamine.php
@@ -16,7 +16,34 @@ $opilased = simplexml_load_file('opilased.xml');
 <?php
 //1.Õpilase nimi
 echo "1.Õpilase nimi: ".$opilased->opilane[0]->nimi;
+//Kõik õpilased
 ?>
+<table>
+    <tr>
+        <th>
+            Õpilase nimi
+        </th>
+        <th>
+            Isikukood
+        </th>
+        <th>
+            Eriala
+        </th>
+        <th>
+            Elukoht
+        </th>
+    </tr>
+    <?php
+    foreach ($opilased->opilane as $opilane ) {
+        echo "<tr>";
+        echo "<td>".$opilane->nimi."</td>";
+        echo "<td>".$opilane->isikukood."</td>";
+        echo "<td>".$opilane->eriala."</td>";
+        echo "<td>".$opilane->elukoht->linn.",".$opilane->elukoht->maakond."</td>";
+        echo "</tr>";
+    }
+    ?>
+</table>
 
 </body>
 </html>
